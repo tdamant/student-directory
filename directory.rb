0adafd1eed6fee05f2ddf5c295a2247f9c8a1dae
Tom Damant <tom@gluon.local>
2018-12-20 17:19:12 +0000

    replace .each_with_index method with a while loop

--- directory.rb
+++ directory.rb
@@ -13,24 +13,39 @@ def input_names
   students
 end
 
-
-def print_header
-  puts "The students of Villains Academy"
+def print_header(letter)
+  puts "The students of Villains Academy with names starting with a #{letter}"
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-    if student[:name][0] == 'S'
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+def choose_starting_letter
+  puts 'print names begging with which letter?'
+  letter = gets.chomp.upcase
+end
+
+def print(students, letter)
+  counter = 0
+  while counter < students.length do
+    student = students[counter]
+    if student[:name][0].upcase == letter
+      puts "#{counter + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+    counter += 1
   end
 end
+#   students.each_with_index do |student, index|
+#     if student[:name][0].upcase == letter
+#       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+#     end
+#   end
+# end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
 students = input_names
-print_header
-print(students)
+letter = choose_starting_letter
+print_header(letter)
+print(students, letter)
 print_footer(students)
