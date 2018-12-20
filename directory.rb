8fa46269b953b2bcc3d2ff973768728568a6a69c
Tom Damant <tom@gluon.local>
2018-12-20 18:22:36 +0000

    refactor user enters cohort and remove user chooses starting letter

--- directory.rb
+++ directory.rb
@@ -16,9 +16,9 @@ def input_cohorts
   ]
   puts 'And now please enter the students cohort'
   cohort = gets.chomp.to_sym
-  while !cohorts.include?(cohort) do
-    puts 'Please re-enter a valid month'
-    cohort = gets.chomp.to_sym
+  if !cohorts.include?(cohort)
+    # December set as default cohort if input not valid
+    cohort = cohorts[0]
   end
   cohort
 end
@@ -37,39 +37,35 @@ def input_names
   students
 end
 
-def print_header(letter)
-  puts "The students of Villains Academy with names starting with a #{letter}".center(50)
+def print_header
+  puts "The students of Villains Academy".center(50)
   puts "-------------".center(50)
 end
 
-def choose_starting_letter
-  puts 'print names begging with which letter?'.center(50)
-  letter = gets.chomp.upcase
-end
-
-def print(students, letter)
+def print(students)
   counter = 0
   while counter < students.length do
     student = students[counter]
-    if student[:name][0].upcase == letter
     puts "#{counter + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
-    end
     counter += 1
   end
 end
-#   students.each_with_index do |student, index|
-#     if student[:name][0].upcase == letter
-#       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-#     end
-#   end
-# end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_names
-letter = choose_starting_letter
-print_header(letter)
-print(students, letter)
+print_header
+print(students)
 print_footer(students)
+
+# def choose_starting_letter
+#   letter = ''
+#   puts 'do you want to print names begging with which letter?'.center(50)
+#   input = gets.chomp.upcase
+#   if input == "YES"
+#     puts 'What letter'
+#     letter = gets.chomp
+#   end
+# end
